b00d36380c28056cecbf82ef21a551051d29657a
Peter Schatteman <prschat@g.clemson.edu>
2014-06-20 23:16:46 -0500

    Atom .h file worked on, still nowhere near complete.

--- atom.h
+++ atom.h
@@ -1,1 +1,16 @@
+#include "plane.h"
 
+class atom{
+	private:
+		int p, n, e;
+		ot coord, v;
+		atom *bonds[4];
+		static double 
+	public:
+		atom();
+		atom(int);//just proton number(creates avg neutron num for element)
+		atom(int, int, int);
+		dist(ot);
+		ot loc();
+		int element();
+		int charge();
